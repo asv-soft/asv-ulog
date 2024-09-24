998655cd5b32df668bd299d7d71d73a539bfccff
Eugene <u03@asv.me>
2024-09-24 19:11:42 +0300

    feat(ulog-file): implement file service and test refactor
    Asana: https://app.asana.com/0/1203851531040615/1208271533462300/f

---
+++ src/Asv.ULog/ULogFile/ULogFileService.cs
@@ -0,0 +1,161 @@
+using System.Buffers;
+using System.Collections;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Text;
+using Asv.ULog.Information;
+using ZLogger;
+
+namespace Asv.ULog;
+
+public class ULogFileService : ULogFileModel
+{
+    private readonly IULogReader _reader;
+    private ReadOnlySequence<byte> _data;
+    public List<ULogParameterMessageToken> Parameters { get; set; } = new();
+
+    public ULogFileService()
+    {
+    }
+
+    private Dictionary<string, ULogType> GetFormatDictionary(ULogFormatMessageToken format)
+    {
+        var dictionary = new Dictionary<string, ULogType>();
+
+        foreach (var field in format.Fields)
+        {
+            if (field.Name.Contains("_padding")) continue;
+            dictionary.Add(field.Name, field.Type.BaseType);
+        }
+
+        foreach (var item in dictionary)
+        {
+            if (item.Value is ULogType.ReferenceType)
+            {
+                var reference = dictionary.FirstOrDefault(_ => _.Key.Equals(item.Value.ToString()));
+                //item = new KeyValuePair<string,ULogType>(item.Key,reference.Value);
+            }
+        }
+
+        return dictionary;
+    }
+
+    private KeyValuePair<string, string> GetInformationDictionary(ULogInformationMessageToken information)
+    {
+        return new KeyValuePair<string, string>(information.Key.Name,
+            ValueToString(information.Key.Type.BaseType, information.Value));
+    }
+    public string GetParameter(ULogParameterMessageToken param)
+    {
+        var result = new StringBuilder();
+        result.Append("Parameter : ");
+        var value = ValueToString(param.Key.Type.BaseType, param.Value);
+        return result.Append($"{param.Key.Name} = {value}").ToString();
+    }
+    private string ValueToString(ULogType type, byte[] value)
+    {
+        switch (type)
+        {
+            case ULogType.UInt32:
+            case ULogType.Int32:
+                return BitConverter.ToInt32(value).ToString(CultureInfo.InvariantCulture);
+            case ULogType.Char:
+                return CharToString(value).ToString();
+            default:
+                throw new ArgumentNullException("Wrong ulog value type for InformationTokenValue");
+        }
+    }
+
+    private ReadOnlySpan<char> CharToString(byte[] value)
+    {
+        var charSize = ULog.Encoding.GetCharCount(value);
+        var charBuffer = new char[charSize];
+        ULog.Encoding.GetChars(value, charBuffer);
+        var rawString = new ReadOnlySpan<char>(charBuffer, 0, charSize);
+        return rawString.ToString();
+    }
+
+    public void Load(ref ReadOnlySequence<byte> data, IULogReader reader)
+    {
+        Definition = new();
+        Definition.Format = new Collection<IDictionary<string, ULogType>>();
+        Definition.Information = new Dictionary<string, string>();
+        Data = new List<IULogToken>();
+        DefinitionAndData = new List<IULogToken>();
+        Unknown = new List<IULogToken>();
+        var rdr = new SequenceReader<byte>(data);
+        while (reader.TryRead(ref rdr, out var token))
+        {
+            switch (token.TokenSection)
+            {
+                case TokenPlaceFlags.Header:
+                    var header = token as ULogFileHeaderToken;
+                    Version = header.Version;
+                    Time = FromTimeStampToDateTime(header.Timestamp).TimeOfDay;
+                    break;
+                case TokenPlaceFlags.Definition:
+                    switch (token.TokenType)
+                    {
+                        case ULogToken.FlagBits:
+                            break;
+                        case ULogToken.Format:
+                            Definition.Format.Add(GetFormatDictionary((ULogFormatMessageToken)token));
+                            break;
+                        case ULogToken.Information:
+                            Definition.Information.Add(GetInformationDictionary((ULogInformationMessageToken)token));
+                            break;
+                    }
+                    break;
+                case (TokenPlaceFlags.DefinitionAndData):
+                    switch (token.TokenType)
+                    {
+                        case ULogToken.Information:
+                            Definition.Information.Add(GetInformationDictionary((ULogInformationMessageToken)token)); 
+                            break;
+                    }
+                    break;
+            }
+        }
+    }
+
+    public ULogFileModel Save()
+    {
+        throw new NotImplementedException();
+    }
+
+    private DateTime FromTimeStampToDateTime(ulong timestamp)
+    {
+        var dt = new DateTime();
+        dt = dt.AddSeconds(timestamp).ToLocalTime();
+        return dt;
+    }
+}
+
+public class ULogFileModel : IULogFile
+{
+    public TimeSpan Time { get; set; }
+    public byte Version { get; set; }
+    public Definition Definition { get; set; }
+
+    public ICollection<IULogToken> Subscription { get; set; }
+    public ICollection<IULogToken> Data { get; set; }
+    public ICollection<IULogToken> DefinitionAndData { get; set; }
+    public ICollection<IULogToken> Unknown { get; set; }
+}
+
+public class Definition
+{
+    public ICollection<IDictionary<string, ULogType>> Format { get; set; }
+    public IDictionary<string, string> Information { get; set; }
+    public IDictionary<string, string> MultiInformation { get; set; }
+}
+
+public interface IULogFile
+{
+    public TimeSpan Time { get; set; }
+    public byte Version { get; set; }
+    public Definition Definition { get; set; }
+    public ICollection<IULogToken> DefinitionAndData { get; set; }
+    public ICollection<IULogToken> Subscription { get; set; }
+    public ICollection<IULogToken> Data { get; set; }
+}
--- src/Asv.Ulog.Tests/ULog/ULogFileService.Tests.cs
+++ src/Asv.Ulog.Tests/ULog/ULogFileService.Tests.cs
@@ -27,106 +27,25 @@ public class ULogFileService_Tests
         _output.WriteLine("===============Header===============");
         _output.WriteLine($"Version: {uLogFileService.Version}");
         _output.WriteLine($"Time: {uLogFileService.Time}");
-        _output.WriteLine($"Is Corrupted {uLogFileService}");
         _output.WriteLine("\n===============Definition===============");
-        //_output.WriteLine($"{result.FlagBits.TokenName}");
-        
         foreach (var definition in uLogFileService.Definition.Format)
         {
+            _output.WriteLine($"Format:");
             foreach (var fields in definition)
             {
                 _output.WriteLine($"{fields.Key} = {fields.Value}");
             }
         }
+        foreach (var info in uLogFileService.Definition.Information)
+        {
+            _output.WriteLine($"{info.Key}: {info.Value}");
+        }
 
         _output.WriteLine("\n===============Definition and Data===============");
-        foreach (var defData in uLogFileService.DefinitionAndData)
-        {
-            switch (defData.TokenType)
-            {
-                case ULogToken.Information:
-                    _output.WriteLine(GetInformation((ULogInformationMessageToken)defData));
-                    break;
-                case ULogToken.Parameter:
-                    _output.WriteLine(GetParameter((ULogParameterMessageToken)defData));
-                    break;
-            }
-        }
 
         foreach (var param in uLogFileService.Parameters)
         {
             _output.WriteLine($"{uLogFileService.Time} {param.TokenName} {param.Value}");
         }
-    }
-
-    private ValueType ParameterTokenValueToValueType(ULogType typeBaseType, byte[] value)
-    {
-        switch (typeBaseType)
-        {
-            case ULogType.Float:
-                var single = BitConverter.ToSingle(value);
-                return single;
-            case ULogType.Int32:
-                var int32 = BitConverter.ToInt32(value);
-                return int32;
-            default:
-                throw new ArgumentException("Wrong ulog value type for ParameterTokenValue");
-        }
-    }
-
-    public string GetParameter(ULogParameterMessageToken param)
-    {
-        var result = new StringBuilder();
-        result.Append("Parameter : ");
-        var value = ValueToString(param.Key.Type.BaseType, param.Value);
-        return result.Append($"{param.Key.Name} = {value}").ToString();
-    }
-
-    public string GetFormat(ULogFormatMessageToken format)
-    {
-        var result = new StringBuilder();
-        result.Append($"{format.TokenName} {format.Fields.Count} items\n [\n");
-        foreach (var field in format.Fields)
-        {
-            result.Append($"{field.Name} = {field.Type.TypeName}\n");
-        }
-
-        result.Append("]");
-        return result.ToString();
-    }
-
-    public string GetInformation(ULogInformationMessageToken information)
-    {
-        var result = new StringBuilder();
-        result.Append($"{information.TokenName} : ");
-        var str = ValueToString(information.Key.Type.BaseType, information.Value);
-        result.Append($"{information.Key.Name} = {str}");
-
-        return result.ToString();
-    }
-
-    private string ValueToString(ULogType type, byte[] value)
-    {
-        switch (type)
-        {
-            case ULogType.UInt32:
-            case ULogType.Int32:
-                return BitConverter.ToInt32(value).ToString(CultureInfo.InvariantCulture);
-            case ULogType.Char:
-                return CharToString(value).ToString();
-            case ULogType.Float:
-                return BitConverter.ToSingle(value).ToString();
-            default:
-                throw new ArgumentNullException("Wrong ulog value type for InformationTokenValue");
-        }
-    }
-
-    private ReadOnlySpan<char> CharToString(byte[] value)
-    {
-        var charSize = ULog.ULog.Encoding.GetCharCount(value);
-        var charBuffer = new char[charSize];
-        ULog.ULog.Encoding.GetChars(value, charBuffer);
-        var rawString = new ReadOnlySpan<char>(charBuffer, 0, charSize);
-        return rawString.ToString();
     }
 }
