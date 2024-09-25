67f8b9efef16e83b98de8f101a0373ce9f8f0ea8
Лев <u05@asv.me>
2024-09-25 14:57:05 +0300

    feat: change names of test ulog files to be more clear about their content Asana: https://app.asana.com/0/1203851531040615/1208388559101533/f

--- src/Asv.Ulog.Tests/ULog/ULogLoggedDataMessageToken.Tests.cs
+++ src/Asv.Ulog.Tests/ULog/ULogLoggedDataMessageToken.Tests.cs
@@ -16,7 +16,7 @@ public class ULogLoggedDataMessageTokenTests
     [Fact]
     public void ReadLoggedDataFromFile()
     {
-        var data = new ReadOnlySequence<byte>(TestData.ulog_log_small);
+        var data = new ReadOnlySequence<byte>(TestData.ulog_sample);
         var rdr = new SequenceReader<byte>(data); 
         var reader = ULog.ULog.CreateReader();
 
